a51563af7175fca5f89a6adbfb8ddab415f450be
LiLQD <luuquangdung1511@gmail.com>
2026-04-18 18:19:05 +0700

    Add Player factory and mark's getter setter

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const Gameboard = (function GameBoard(){
-    const arr = [[firstSquare, secondSquare, thirdSquare], [forthSquare, fifthSquare ,sixthSquare], [seventhSquare, eighthSquare, ninethSquare]];
+    
+    const arr = [["", "", ""], ["", "" ,""], ["", "", ""]];
     function getSquareValue(row, column){
         return arr[row][column];
     }
@@ -8,3 +9,40 @@ const Gameboard = (function GameBoard(){
     }
     return {getSquareValue, setSquareValue};
 })()
+
+function Player(name){
+    let point = 0;
+    let usedMark = new Set();
+    let mark;
+    function checkUsedMark(setMark){
+        if(usedMark.has(setMark)){
+            return true;
+        }
+    }
+    function win(){
+        point++;
+        console.log(`The winner is ${name} with ${point} points`);
+    }
+    function setMarkX(){
+        if(checkUsedMark("X") === true){
+            console.log("X mark is already in use, you are Y mark");
+            mark = "Y";
+        }
+        else{
+            mark = "X";
+        }
+    }
+    function setMarkY(){
+        if(checkUsedMark("Y") === true){
+            console.log("Y mark is already in use, you are X mark");
+            mark = "X";
+        }
+        else{
+            mark = "Y";
+        }
+    }
+    function getMark(){
+        return mark;
+    }
+    return {win, setMarkX, setMarkY, getMark};
+}
