85f324535f54fe14825270a5a4b7184f532a30ce
LiLQD <luuquangdung1511@gmail.com>
2026-04-22 21:35:02 +0700

    feat(gamecontroller): add drawCheck function to GameController factory

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function Player(name) {
   }
   function win() {
     point++;
-    console.log(`{name} won with {point} points`);
+    console.log(`${name} won with ${point} points`);
   }
   return { name, win, setMark, getMark };
 }
@@ -65,35 +65,36 @@ const gameController = (function GameController() {
   console.log(player2);
 
   let isFirstPlayerTurn = true;
+  
   function switchTurn() {
     return (isFirstPlayerTurn = isFirstPlayerTurn ? false : true);
   }
-  
+
   function winCheck() {
     const valueBoard = Gameboard.getBoard();
     const boardSize = valueBoard.length;
     let diagWinCondition = true;
     let antiDiagWinCondition = true;
-    let firstDiagCell = valueBoard[0][0].getCell();
-      if (firstDiagCell === "") diagWinCondition = false;
+    let firstDiagCell = valueBoard[0][0];
+    if (firstDiagCell === "") diagWinCondition = false;
 
-      let firstAntiDiagCell = valueBoard[0][boardSize - 1].getCell();
-      if (firstAntiDiagCell === "") antiDiagWinCondition = false;
+    let firstAntiDiagCell = valueBoard[0][boardSize - 1];
+    if (firstAntiDiagCell === "") antiDiagWinCondition = false;
     for (let n = 0; n < boardSize; n++) {
       let rowWinCondition = true;
       let colWinCondition = true;
 
-      let firstRowCell = valueBoard[n][0].getCell();
+      let firstRowCell = valueBoard[n][0];
       if (firstRowCell === "") rowWinCondition = false;
 
-      let firstColCell = valueBoard[0][n].getCell();
+      let firstColCell = valueBoard[0][n];
       if (firstColCell === "") colWinCondition = false;
 
       for (let m = 0; m < boardSize; m++) {
-        if (rowWinCondition && valueBoard[n][m].getCell() != firstRowCell) {
+        if (rowWinCondition && valueBoard[n][m] != firstRowCell) {
           rowWinCondition = false;
         }
-        if (colWinCondition && valueBoard[m][n].getCell() != firstColCell) {
+        if (colWinCondition && valueBoard[m][n] != firstColCell) {
           colWinCondition = false;
         }
       }
@@ -101,24 +102,35 @@ const gameController = (function GameController() {
       if (rowWinCondition || colWinCondition) return true;
     }
     for (let i = 0; i < boardSize; i++) {
-      if (diagWinCondition && valueBoard[i][i].getCell() != firstDiagCell) {
+      if (diagWinCondition && valueBoard[i][i] != firstDiagCell) {
         diagWinCondition = false;
       }
 
       if (
         antiDiagWinCondition &&
-        valueBoard[i][boardSize - 1 - i].getCell() != firstAntiDiagCell
+        valueBoard[i][boardSize - 1 - i] != firstAntiDiagCell
       ) {
         antiDiagWinCondition = false;
       }
     }
     if (diagWinCondition || antiDiagWinCondition) return true;
+    return false;
   }
-
+  function drawCheck() {
+    const valueBoard = Gameboard.getBoard();
+    const boardSize = valueBoard.length;
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        if (valueBoard[i][j] === "") return false;
+      }
+    }
+    return true;
+  }
   function playRound() {
-    while (!isWin) {
-      winCheck();
+    let currentPlayer;
+    while (!winCheck() && !drawCheck()) {
       if (isFirstPlayerTurn) {
+        currentPlayer = player1;
         console.log(Gameboard.getBoard());
         console.log(`Is ${player1.name} turn`);
         const rowIndex = prompt("Row index");
@@ -127,6 +139,7 @@ const gameController = (function GameController() {
         Gameboard.placeMark(rowIndex - 1, columnIndex - 1, player1.getMark());
         switchTurn();
       } else {
+        currentPlayer = player2;
         console.log(Gameboard.getBoard());
         console.log(`Is ${player2.name} turn`);
         const rowIndex = prompt("Row index");
@@ -136,6 +149,10 @@ const gameController = (function GameController() {
         switchTurn();
       }
     }
+    console.log(Gameboard.getBoard());
+    if (winCheck()) {
+      currentPlayer.win();
+    } else console.log("Draw");
   }
   return { playRound };
 })();
